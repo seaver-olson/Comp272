84de6034afea4da7adf6a90bd9d671b6efde4be4
seaver-olson <sjolson007@gmail.com>
2024-08-30 13:44:11 -0500

    worked on stack exercise

--- StackExercise.java
+++ StackExercise.java
@@ -1,10 +1,27 @@
 import java.util.Stack;
 
 public class StackExercise {
-    
+    public static void getReverse(Stack<Integer> stack){
+	int[stack.size] nums = new int[stack.size];
+	for (int i = 0; i < nums.length(); i++){
+		nums[i] = stack.pop();
+		if (i = nums.length-1){
+			System.out.println(nums[i]);
+		} else{
+			System.out.println(nums[i] + ", ");
+			}
+	}
+	for (int i = nums.length() -1; i >= 0; i++){
+		stack.push(nums[i]);	
+	}
+    }
+    public static void insertAt(Stack<Integer> stack, int val, int index){
+	int[stack.size] nums = new int[stack.size];
+	
+    }
     public static void main(String[] args){
         //create Stack using built in Java class
-        
+	Stack<Integer> stack = new Stack<Integer>        
         //push elements into stack
         
         //call methods
